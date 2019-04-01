c2afb4668e7589bbb1bdf38e6d7e045ce53d13e8
karinadevetry <karina@devetry.com>
2019-04-01 11:44:52 -0600

    building_creation: add private method, create_space_per_floor

--- lib/urbanopt/geojson/building_creation.rb
+++ lib/urbanopt/geojson/building_creation.rb
@@ -1,7 +1,6 @@
 module URBANopt
   module GeoJSON
     module BuildingCreation
-
       def self.create_building(feature, create_method, model, origin_lat_lon, runner, zoning=false)
       ##
       # Returns an array of instances of OpenStudio::Model::Space
@@ -63,7 +62,7 @@ module URBANopt
         if create_method == :space_per_floor or create_method == :spaces_per_floor
           if space_type
             # get the building use and fix any issues
-            building_space_type = create_space_type(space_type, space_type, model)
+            building_space_type = URBANopt::GeoJSON::Helper.create_space_type(space_type, space_type, model)
             model.getBuilding.setSpaceType(building_space_type)
             model.getBuilding.setStandardsBuildingType(space_type)
             model.getBuilding.setRelocatable(false)
@@ -79,7 +78,7 @@ module URBANopt
         spaces = []
         if create_method == :space_per_floor or create_method == :spaces_per_floor
           (-number_of_stories_below_ground+1..number_of_stories_above_ground).each do |story_number|
-            new_spaces = create_space_per_floor(feature.feature_json, story_number, floor_to_floor_height, model, origin_lat_lon, runner, zoning)
+            new_spaces = create_space_per_floor(feature, story_number, floor_to_floor_height, model, origin_lat_lon, runner, zoning)
             spaces.concat(new_spaces)
           end
         elsif create_method == :space_per_building
@@ -88,7 +87,73 @@ module URBANopt
         return spaces
       end
 
+      def self.create_space_per_floor(feature, story_number, floor_to_floor_height, model, origin_lat_lon, runner, zoning=false)
+      ##
+      # Returns an array of instances of OpenStudio::Model::Space per floor
+      # NOTE: update this return value once test is made more specific
+      #
+      # Params:
+      # - building_json: building json object (examples in nrel_stm_footprints.geojson)
+      # - story_number: number amount of floors in building
+      # - floor_to_floor_height: number number indicating height of building stories
+      # - model: instance of OpenStudio::Model::Model
+      # - origin_lat_lon: instance of OpenStudio::PointLatLon indicating origin lat & lon
+      # - zoning: zoning is true if you'd like to utilize aspects of function that are specific to zoning
+        geometry = feature.feature_json[:geometry]
+        properties = feature.feature_json[:properties]
+        floor_prints = []
+        multi_polygons = feature.get_multi_polygons()
+        multi_polygons.each do |multi_polygon|
+          if story_number == 1 && multi_polygon.size > 1
+            runner.registerWarning("Ignoring holes in polygon")
+          end
+          multi_polygon.each do |polygon|
+            elevation = (story_number-1)*floor_to_floor_height
+            floor_print = URBANopt::GeoJSON::Helper.floor_print_from_polygon(polygon, elevation, origin_lat_lon, runner, zoning)
+            if floor_print
+              if zoning
+                this_floor_prints = divide_floor_print(floor_print, 4.0, runner)
+                floor_prints.concat(this_floor_prints)
+              else
+                floor_prints << floor_print
+              end
+            else
+              runner.registerWarning("Cannot create story #{story_number}")
+            end
+            # subsequent polygons are holes, we do not support them
+            break
+          end
+        end
+        result = []
+        floor_prints.each do |floor_print|
+          space = OpenStudio::Model::Space.fromFloorPrint(floor_print, floor_to_floor_height, model)
+          if space.empty?
+            runner.registerWarning("Cannot create space for story #{story_number}")
+            next
+          end
+          space = space.get
+          space.setName("Building Story #{story_number} Space")
+          space.surfaces.each do |surface|
+            if surface.surfaceType == "Wall"
+              if story_number < 1
+                surface.setOutsideBoundaryCondition("Ground")
+              end
+            end
+          end
+          building_story = OpenStudio::Model::BuildingStory.new(model)
+          building_story.setName("Building Story #{story_number}")
+          space.setBuildingStory(building_story)
+          thermal_zone = OpenStudio::Model::ThermalZone.new(model)
+          thermal_zone.setName("Building Story #{story_number} ThermalZone")
+          space.setThermalZone(thermal_zone)
+          result << space
+        end
+        return result
+      end
 
+      class << self
+        private :create_space_per_floor
+      end
     end
   end
 end
