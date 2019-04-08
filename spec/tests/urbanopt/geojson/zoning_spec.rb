bdac1f961c1a42e736f2a47c8a8c3e66edadd3d9
karinadevetry <karina@devetry.com>
2019-04-08 11:36:46 -0600

    zoning_spec: add commented out zoning test

--- spec/tests/urbanopt/geojson/zoning_spec.rb
+++ spec/tests/urbanopt/geojson/zoning_spec.rb
@@ -49,4 +49,24 @@ RSpec.describe URBANopt::GeoJSON do
     expect(divided_floorprint[0][0].class).to eq(OpenStudio::Point3d)
   end
 
+    #   it 'creates a zoning floorprint from polygon' do
+    #   # REVISIT: WHY ZONING SET TO TRUE 
+    #   polygon = [
+    #     [1, 5],
+    #     [5, 5],
+    #     [5, 1],
+    #   ]
+    #   floorprint = @gem_instance.floor_print_from_polygon(polygon, 0, @origin_lat_lon, @runner, true)
+    #   vertex1 = OpenStudio::Point3d.new(555807.1692993665, 110568.77482456664, 0)
+    #   vertex2 = OpenStudio::Point3d.new(110893.07603825576, 552183.9600277696, 0)
+    #   vertex3 = OpenStudio::Point3d.new(553790.0141403697, 552183.9600277696, 0)
+    #   vertexes = [vertex1, vertex2, vertex3]
+    #   vertexes.each_with_index {
+    #     |vertex, idx|
+    #     expect(floorprint[idx].x).to eq(vertex.x)
+    #     expect(floorprint[idx].y).to eq(vertex.y)
+    #     expect(floorprint[idx].z).to eq(vertex.z)
+    #   }
+    # end
+
 end
