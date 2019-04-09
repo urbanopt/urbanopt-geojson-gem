e72c9a39887bfc99a3e6268387d8d3ffa0286e83
Brian Schiller <bgschiller@gmail.com>
2019-04-09 13:11:28 -0600

    flat courtyard should not shadow

--- lib/measures/urban_geometry_creation/tests/shadowed_tests.rb
+++ lib/measures/urban_geometry_creation/tests/shadowed_tests.rb
@@ -29,4 +29,27 @@ class UrbanGeometryCreationTest < MiniTest::Unit::TestCase
         courtyard_shadows_emu = URBANopt::GeoJSON::Helper.is_shadowed(courtyard, red_kangaroo_emu, denver_zoo)
         assert courtyard_shadows_emu, "Expected courtyard at 30m tall to shadow the emu"
     end
+
+    def test_flat_courtyard_does_not_shadow
+        denver_zoo = OpenStudio::PointLatLon.new(39.749242, -104.951024, 0);
+        courtyard = [
+            OpenStudio::Point3d.new(-5, 20, 0),
+            OpenStudio::Point3d.new(-10, 5, 0),
+            OpenStudio::Point3d.new(15, -10, 0),
+            OpenStudio::Point3d.new(25, 10, 0),
+            OpenStudio::Point3d.new(-5, 20, 0),
+        ]
+        red_kangaroo_emu = [
+            OpenStudio::Point3d.new(-10, 15, 30),
+            OpenStudio::Point3d.new(-5, 25, 30),
+            OpenStudio::Point3d.new(10, 20, 30),
+            OpenStudio::Point3d.new(15, 45, 30),
+            OpenStudio::Point3d.new(-10, 55, 30),
+            OpenStudio::Point3d.new(-25, 25, 30),
+            OpenStudio::Point3d.new(-10, 15, 30),
+        ]
+
+        courtyard_shadows_emu = URBANopt::GeoJSON::Helper.is_shadowed(courtyard, red_kangaroo_emu, denver_zoo)
+        assert !courtyard_shadows_emu, "Expected courtyard at 0m tall not to shadow the emu"
+    end
 end
