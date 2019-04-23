f97e0f5fd59ab0de684cb688ecb3c23685e0b909
karinadevetry <karina@devetry.com>
2019-04-23 16:29:08 -0600

    building.rb: update create_other_buildings to handle surrounding buildings - NOTE: surrounding building json (feature_collectioon) is stubbed out

--- lib/urbanopt/geojson/building.rb
+++ lib/urbanopt/geojson/building.rb
@@ -1,7 +1,6 @@
 module URBANopt
   module GeoJSON
     class Building < Feature
-
       ##
       # Returns an array of instances of OpenStudio::Model::Space
       #
@@ -12,20 +11,18 @@ module URBANopt
       # * +origin_lat_lon+ instance of OpenStudio::PointLatLon indicating origin lat & lon
       # * +runner+ measure run's instance of OpenStudio::Measure::OSRunner
       # * +zoning+ Boolean, is true if you'd like to utilize aspects of function that are specific to zoning
-      def create_building(create_method, model, origin_lat_lon, runner, zoning=false)
-        # puts "FEATTT", @feature_json
-        number_of_stories = @feature_json[:properties][:number_of_stories]
-        number_of_stories_above_ground = @feature_json[:properties][:number_of_stories_above_ground]
-        number_of_stories_below_ground = @feature_json[:properties][:number_of_stories_below_ground]
-        number_of_residential_units = @feature_json[:properties][:number_of_residential_units]
-        space_type = @feature_json[:properties][:building_type]
-        puts "SPACE TYPEE", space_type
+      def create_building(create_method, model, origin_lat_lon, runner, zoning=false, other_building=@feature_json)
+        number_of_stories = other_building[:properties][:number_of_stories]
+        number_of_stories_above_ground = other_building[:properties][:number_of_stories_above_ground]
+        number_of_stories_below_ground = other_building[:properties][:number_of_stories_below_ground]
+        number_of_residential_units = other_building[:properties][:number_of_residential_units]
+        space_type = other_building[:properties][:building_type]
         if zoning
-          surface_elevation	= @feature_json[:properties][:surface_elevation]
-          roof_elevation	= @feature_json[:properties][:roof_elevation]
-          floor_to_floor_height = @feature_json[:properties][:floor_to_floor_height]
+          surface_elevation	= other_building[:properties][:surface_elevation]
+          roof_elevation	= other_building[:properties][:roof_elevation]
+          floor_to_floor_height = other_building[:properties][:floor_to_floor_height]
         else
-          maximum_roof_height = @feature_json[:properties][:maximum_roof_height]
+          maximum_roof_height = other_building[:properties][:maximum_roof_height]
         end
         # if space_type == "Mixed use"
         #   mixed_types = []
@@ -84,7 +81,7 @@ module URBANopt
             spaces.concat(new_spaces)
           end
         elsif create_method == :space_per_building
-          spaces = create_space_per_building(-number_of_stories_below_ground*floor_to_floor_height, number_of_stories_above_ground*floor_to_floor_height, model, runner, zoning)
+          spaces = create_space_per_building(-number_of_stories_below_ground*floor_to_floor_height, number_of_stories_above_ground*floor_to_floor_height, model, origin_lat_lon, runner, zoning)
         end
         return spaces
       end
@@ -93,14 +90,13 @@ module URBANopt
       # Returns an array of instances of OpenStudio::Model::Space
       #
       # [Params]
-      # * +feature+ instance of Feature class madde with geojson file
       # * +surrounding_buildings+ building json object for surrounding buildings
       # * +model+ instance of OpenStudio::Model::Model
       # * +origin_lat_lon+ instance of OpenStudio::PointLatLon indicating origin lat & lon
       # * +runner+ measure run's instance of OpenStudio::Measure::OSRunner
-      def create_other_buildings(feature, surrounding_buildings, model, origin_lat_lon, runner)
-        project_id = feature.feature_json[:properties][:project_id]
-        feature_id = feature.feature_json[:properties][:id]
+      def create_other_buildings(surrounding_buildings, model, origin_lat_lon, runner, zoning=false)
+        project_id = @feature_json[:properties][:project_id]
+        feature_id = @feature_json[:properties][:id]
         # nearby buildings to conver to shading
         convert_to_shades = []
         # query for nearby buildings
@@ -109,18 +105,25 @@ module URBANopt
         params[:feature_id] = feature_id
         params[:distance] = 100
         params[:proximity_feature_types] = ['Building']
-        feature_collection = get_feature_collection(params)
+
+        # NOTE: SURROUNDING BUILDING JSON IS STUBBED OUT
+        feature_collection = {
+          "features": [
+            "type":"Feature","geometry":{"type":"Polygon","coordinates":[[[-105.1712420582771,39.743195219730666],[-105.17083168029784,39.74345301902031],[-105.16999751329422,39.74261774582203],[-105.17005383968352,39.7425641229992],[-105.169957280159,39.742469251748986],[-105.16988754272462,39.742491938364225],[-105.16976416110992,39.74227332157966],[-105.16981244087218,39.742254759745265],[-105.16978025436399,39.74207120355811],[-105.16938596963882,39.74213307648483],[-105.16932964324953,39.74194951997353],[-105.170314013958,39.74180514934022],[-105.17034888267514,39.74198251893293],[-105.16999751329422,39.74204439193929],[-105.17002433538435,39.742159888069125],[-105.1701021194458,39.74212688919465],[-105.1703569293022,39.74207326598989],[-105.17048299312592,39.74223001062495],[-105.17037034034726,39.742306320384046],[-105.17020672559738,39.742380567636104],[-105.17024427652358,39.74243212818075],[-105.17038106918336,39.742343444020094],[-105.1712420582771,39.743195219730666]]]},"properties":{"id":"5caf78834b5dca7c6d8f3e3b","source_id":"Energy Systems Integration Facility","source_name":"NREL_GDS","project_id":"5caf78404b5dca7c6d8f3e0a","stroke":"#555555","stroke-width":2,"stroke-opacity":1,"fill":"#555555","fill-opacity":0.5,"name":"Energy Systems Integration Facility","maximum_roof_height":30,"floor_area":296826.11368011055,"number_of_stories":3,"number_of_stories_above_ground":3,"building_type":"Office","surface_elevation":5198,"type":"Building","footprint_area":99064,"footprint_perimeter":2194,"updated_at":"2017-09-01T21:17:07.447Z","created_at":"2017-09-01T21:16:27.579Z","height":9,"geometryType":"Polygon"}
+          ]
+        }
+
         if feature_collection[:features].nil?
           runner.registerWarning("No features found in #{feature_collection}")
           return []
         end
         # get first floor footprint points
         building_points = []
-        multi_polygons = feature.get_multi_polygons(feature)
+        multi_polygons = get_multi_polygons()
         multi_polygons.each do |multi_polygon|
           multi_polygon.each do |polygon|
             elevation = 0
-            floor_print = floor_print_from_polygon(polygon, elevation, origin_lat_lon, runner)
+            floor_print = URBANopt::GeoJSON::Helper.floor_print_from_polygon(polygon, elevation, origin_lat_lon, runner, zoning)
             floor_print.each do |point|
               building_points << point
             end
@@ -138,7 +141,7 @@ module URBANopt
             roof_elevation	= other_building[:properties][:roof_elevation]
             number_of_stories = other_building[:properties][:number_of_stories]
             number_of_stories_above_ground = other_building[:properties][:number_of_stories_above_ground]
-            maximum_roof_height = properties[:maximum_roof_height]
+            maximum_roof_height = other_building[:properties][:maximum_roof_height]
             if number_of_stories_above_ground.nil?
               if number_of_stories_below_ground.nil?
                 number_of_stories_above_ground = number_of_stories
@@ -150,7 +153,8 @@ module URBANopt
             floor_to_floor_height = 3
             if number_of_stories_above_ground && number_of_stories_above_ground > 0 && maximum_roof_height
               floor_to_floor_height = maximum_roof_height / number_of_stories_above_ground
-              floor_to_floor_height = OpenStudio::convert(floor_to_floor_height, 'ft', 'm')
+              # COMMENTED OUT THIS LINE: CAN'T TREAT OPENSTUDIO INSTANCE LIKE FIXNUM 
+              # floor_to_floor_height = OpenStudio::convert(floor_to_floor_height, 'ft', 'm')
             end
             other_height = number_of_stories_above_ground * floor_to_floor_height
             # get first floor footprint points
@@ -158,7 +162,7 @@ module URBANopt
             multi_polygons = get_multi_polygons(other_building)
             multi_polygons.each do |multi_polygon|
               multi_polygon.each do |polygon|
-                floor_print = floor_print_from_polygon(polygon, other_height, origin_lat_lon, runner)
+                floor_print = URBANopt::GeoJSON::Helper.floor_print_from_polygon(polygon, other_height, origin_lat_lon, runner, zoning)
                 floor_print.each do |point|
                   other_building_points << point
                 end
@@ -171,7 +175,7 @@ module URBANopt
               next
             end
           end
-          other_spaces = create_building(other_building, :space_per_building, model, runner)
+          other_spaces = create_building(:space_per_building, model, origin_lat_lon, runner, zoning, other_building)
           if other_spaces.nil? || other_spaces.empty?
             runner.registerWarning("Failed to create spaces for other building '#{name}'")
           end
@@ -208,7 +212,7 @@ module URBANopt
               runner.registerWarning("Ignoring holes in polygon")
             end
             multi_polygon.each do |polygon|
-              floor_print = floor_print_from_polygon(polygon, min_elevation, origin_lat_lon, runner, zoning)
+              floor_print = URBANopt::GeoJSON::Helper.floor_print_from_polygon(polygon, min_elevation, origin_lat_lon, runner, zoning)
               if floor_print
                 floor_prints << floor_print
               else
