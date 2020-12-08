29f21bdfebd738172f35468323396ea7ceb35bb0
Nathan Moore <nathan.moore@nrel.gov>
2020-12-08 16:22:56 -0700

    changes from rake update_measures

--- lib/urbanopt/geojson/scale_area.rb
+++ lib/urbanopt/geojson/scale_area.rb
@@ -1,5 +1,5 @@
 # *********************************************************************************
-# URBANopt (tm), Copyright (c) 2019-2020, Alliance for Sustainable Energy, LLC, and other
+# URBANopt™, Copyright (c) 2019-2020, Alliance for Sustainable Energy, LLC, and other
 # contributors. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification,
@@ -27,6 +27,7 @@
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 # *********************************************************************************
+
 require 'json'
 require 'net/http'
 require 'uri'
